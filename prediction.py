bd8e9a09143de1e3ef3000f871a21aec84e4cc79
chihuangamo <36184984+chihuangamo@users.noreply.github.com>
2019-06-30 10:48:44 +0800

    Add files via upload

--- prediction.py
+++ prediction.py
@@ -11,19 +11,78 @@ os.chdir("C:/Users/User/Documents/Codes/IMDB")
 import pandas as pd
 movie = pd.read_csv("movie_good_or_bad.csv")
 
-#%% CountVectorizer
+#%%
 from sklearn.feature_extraction.text import CountVectorizer 
-vec = CountVectorizer()
-vec.fit(movie.crews) 
-vec.get_feature_names()
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV, StratifiedKFold
+from sklearn.pipeline import Pipeline
+from sklearn.feature_selection import SelectKBest, SelectPercentile
+from sklearn.feature_selection import chi2
+from sklearn.metrics import f1_score, confusion_matrix, accuracy_score
+
+X_train, X_test, y_train, y_test = train_test_split(movie.crews,
+                                                    movie.good_or_bad,
+                                                    test_size = 0.2)                                                    )
+accuracy_list = []
+f1_list = []
+for p in range(10, 101, 10):
+    pipeline = Pipeline([
+        ('vect', CountVectorizer()),
+        ('chi_2', SelectPercentile(chi2, percentile=p)),
+        ('clf', SGDClassifier())
+        ])
+    
+    pipeline.fit(X_train, y_train)
+    y_pred = pipeline.predict(X_test)
+    
+    accuracy = accuracy_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+    
+    accuracy_list.append(accuracy)
+    f1_list.append(f1)
+    
+print(accuracy_list)
+print(f1_list)
 
 #%%
-from sklearn.linear_model import SGDClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
+import numpy as np
+skf =  StratifiedKFold(n_splits = 4)
+all_accuracy_list = []
+all_f1_list = []
+for train, test in skf.split(movie.crews, movie.good_or_bad):
+    accuracy_list = []
+    f1_list = []
+    for p in range(10, 101, 10):
+        pipeline = Pipeline([
+            ('vect', CountVectorizer()),
+            ('chi_2', SelectPercentile(chi2, percentile=p)),
+            ('clf', SGDClassifier())
+            ])
+        pipeline.fit(X_train, y_train)
+        y_pred = pipeline.predict(X_test)
+        
+        accuracy = accuracy_score(y_test, y_pred)
+        f1 = f1_score(y_test, y_pred)
+        
+        accuracy_list.append(accuracy)
+        f1_list.append(f1)
+    all_accuracy_list.append(accuracy_list)
+    all_f1_list.append(f1_list)
 
-X_train, X_test, y_train, y_test = train_test_split(X =  movie[]
-                                                    y = 
-                                                    test_size=0.3,                                                    )
-                                                        
- 
+acc_array = np.array(all_accuracy_list)
+np.mean(acc_array, axis = 0)    
+#%%
+chi_2 = SelectPercentile(chi2)
+param_grid = [{'chi_2__percentile': list(range(10, 101, 10))}]
+
+pipeline = Pipeline([
+        ('vect', CountVectorizer()),
+        ('chi_2', chi_2),
+        ('clf', SGDClassifier())
+        ])
+
+gscv = GridSearchCV(pipeline, cv=5, param_grid=param_grid)
+gscv.fit(movie.crews, movie.good_or_bad)
+
+results = pd.DataFrame(gscv.cv_results_)
+gscv.best_estimator_ 
